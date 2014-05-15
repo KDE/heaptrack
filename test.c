e109bbd428a27491845777bca3ed5d98dffe6736
Milian Wolff <mail@milianw.de>
2014-05-15 16:09:58 +0200

    extend test.c a bit.

--- test.c
+++ test.c
@@ -6,9 +6,11 @@ int main()
     int i;
     // make app deterministic
     srand(0);
+    void* p = malloc(1);
     for (i = 0; i < 10000; ++i) {
         malloc(rand() % 1000);
         usleep(100);
     }
+    free(p);
     return 0;
 }
