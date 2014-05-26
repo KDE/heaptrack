5cdc129b13a05d2d8374ee7c708722531ee5c981
Milian Wolff <mail@milianw.de>
2014-05-26 21:08:28 +0200

    Add a threaded test application

---
+++ tests/threaded.cpp
@@ -0,0 +1,40 @@
+#include <thread>
+#include <future>
+
+using namespace std;
+
+const int ALLOCS_PER_THREAD = 1000;
+
+int** alloc()
+{
+    int** block = new int*[ALLOCS_PER_THREAD];
+    for (int i = 0; i < ALLOCS_PER_THREAD; ++i) {
+        block[i] = new int;
+    }
+    return block;
+}
+
+bool dealloc(future<int**>&& f)
+{
+    int** block = f.get();
+    for (int i = 0; i < ALLOCS_PER_THREAD; ++i) {
+        delete block[i];
+    }
+    delete block;
+    return true;
+}
+
+int main()
+{
+    for (int i = 0; i < 100; ++i) {
+        auto f1 = async(launch::async, alloc);
+        auto f2 = async(launch::async, alloc);
+        auto f3 = async(launch::async, alloc);
+        auto f4 = async(launch::async, alloc);
+        auto f5 = async(launch::async, dealloc, move(f1));
+        auto f6 = async(launch::async, dealloc, move(f2));
+        auto f7 = async(launch::async, dealloc, move(f3));
+        auto f8 = async(launch::async, dealloc, move(f4));
+    }
+    return 0;
+}
