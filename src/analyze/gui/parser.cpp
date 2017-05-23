KDE/heaptrack

commit 1b9a59dcc625b400e896fae92542496deb472abd

Show inlined frames
This is a major usability improvement for C++ code. Previously,
when we profiled code with heaptrack that was built with compiler
optimizations enabled, most allocations within container classes
where missing some parent frames. Even more confusing, sometimes
we missed the interesting user frames completely, when they
got inlined.

Now, we add all inlined frames to the heaptrack_print output and
also include them in the heaptrack_gui models. The simple
inlining example added by this pach clearly shows the advantage:

Before all we got is this:

1 calls to allocation functions with 1.23KB peak consumption from
main
  at /home/milian/projects/src/heaptrack/tests/manual/inlining.cpp:18
  in /home/milian/projects/build/heaptrack/tests/manual/inlining

Afterwards, we get all frames:

1 calls to allocation functions with 1.23KB peak consumption from
asdf()
  at /home/milian/projects/src/heaptrack/tests/manual/inlining.cpp:3
  in /home/milian/projects/build/heaptrack/tests/manual/inlining
bar()
  at /home/milian/projects/src/heaptrack/tests/manual/inlining.cpp:8
foo()
  at /home/milian/projects/src/heaptrack/tests/manual/inlining.cpp:13
main
  at /home/milian/projects/src/heaptrack/tests/manual/inlining.cpp:18

src/analyze/gui/parser.cpp
@@ -36,20 +36,20 @@ namespace {
 // TODO: use QString directly
 struct StringCache
 {
-    QString func(const InstructionPointer& ip) const
+    QString func(const Frame& frame) const
     {
-        if (ip.functionIndex) {
+        if (frame.functionIndex) {
             // TODO: support removal of template arguments
-            return stringify(ip.functionIndex);
+            return stringify(frame.functionIndex);
         } else {
             return unresolvedFunctionName();
         }
     }
 
-    QString file(const InstructionPointer& ip) const
+    QString file(const Frame& frame) const
     {
-        if (ip.fileIndex) {
-            return stringify(ip.fileIndex);
+        if (frame.fileIndex) {
+            return stringify(frame.fileIndex);
         } else {
             return {};
         }
@@ -74,19 +74,26 @@ struct StringCache
         // first try a fast index-based lookup
         auto& location = m_locationsMap[index];
         if (!location) {
-            // slow-path, look for interned location
-            // note that we can get the same locatoin for different IPs
-            LocationData data = {func(ip), file(ip), module(ip), ip.line};
-            auto it = lower_bound(m_locations.begin(), m_locations.end(), data);
-            if (it != m_locations.end() && **it == data) {
-                // we got the location already from a different ip, cache it
-                location = *it;
-            } else {
-                // completely new location, cache it in both containers
-                auto interned = make_shared<LocationData>(data);
-                m_locations.insert(it, interned);
-                location = interned;
-            }
+            return frameLocation(ip.frame, ip.moduleIndex);
+        }
+        return location;
+    }
+
+    LocationData::Ptr frameLocation(const Frame& frame, const ModuleIndex& moduleIndex) const
+    {
+        LocationData::Ptr location;
+        // slow-path, look for interned location
+        // note that we can get the same location for different IPs
+        LocationData data = {func(frame), file(frame), stringify(moduleIndex), frame.line};
+        auto it = lower_bound(m_locations.begin(), m_locations.end(), data);
+        if (it != m_locations.end() && **it == data) {
+            // we got the location already from a different ip, cache it
+            location = *it;
+        } else {
+            // completely new location, cache it in both containers
+            auto interned = make_shared<LocationData>(data);
+            m_locations.insert(it, interned);
+            location = interned;
         }
         return location;
     }
@@ -180,7 +187,7 @@ struct ParserData final : public AccumulatedTraceData
                 }
                 const auto ip = alloc.ip;
                 (labelIds[ip].*label) = i + 1;
-                const auto function = stringCache.func(findIp(ip));
+                const auto function = stringCache.func(findIp(ip).frame);
                 data->labels[i + 1] = function;
             }
         };
@@ -308,6 +315,15 @@ void setParents(QVector<RowData>& children, const RowData* parent)
 TreeData mergeAllocations(const ParserData& data)
 {
     TreeData topRows;
+    auto addRow = [](TreeData* rows, const LocationData::Ptr& location, const Allocation& cost) -> TreeData* {
+        auto it = lower_bound(rows->begin(), rows->end(), location);
+        if (it != rows->end() && it->location == location) {
+            it->cost += cost;
+        } else {
+            it = rows->insert(it, {cost, location, nullptr, {}});
+        }
+        return &it->children;
+    };
     // merge allocations, leave parent pointers invalid (their location may change)
     for (const auto& allocation : data.allocations) {
         auto traceIndex = allocation.traceIndex;
@@ -316,18 +332,15 @@ TreeData mergeAllocations(const ParserData& data)
             const auto& trace = data.findTrace(traceIndex);
             const auto& ip = data.findIp(trace.ipIndex);
             auto location = data.stringCache.location(trace.ipIndex, ip);
-
-            auto it = lower_bound(rows->begin(), rows->end(), location);
-            if (it != rows->end() && it->location == location) {
-                it->cost += allocation;
-            } else {
-                it = rows->insert(it, {allocation, location, nullptr, {}});
+            rows = addRow(rows, location, allocation);
+            for (const auto& inlined : ip.inlined) {
+                auto inlinedLocation = data.stringCache.frameLocation(inlined, ip.moduleIndex);
+                rows = addRow(rows, inlinedLocation, allocation);
             }
-            if (data.isStopIndex(ip.functionIndex)) {
+            if (data.isStopIndex(ip.frame.functionIndex)) {
                 break;
             }
             traceIndex = trace.parentIndex;
-            rows = &it->children;
         }
     }
     // now set the parents, the data is constant from here on

CMakeLists.txt
@@ -18,7 +18,7 @@ set(HEAPTRACK_VERSION_MINOR 0)
 set(HEAPTRACK_VERSION_PATCH 0)
 set(HEAPTRACK_LIB_VERSION 1.0.0)
 set(HEAPTRACK_LIB_SOVERSION 1)
-set(HEAPTRACK_FILE_FORMAT_VERSION 1)
+set(HEAPTRACK_FILE_FORMAT_VERSION 2)
 
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
 

src/analyze/accumulatedtracedata.cpp
@@ -208,11 +208,20 @@ bool AccumulatedTraceData::read(istream& in)
             InstructionPointer ip;
             reader >> ip.instructionPointer;
             reader >> ip.moduleIndex;
-            reader >> ip.functionIndex;
-            reader >> ip.fileIndex;
-            reader >> ip.line;
+            auto readFrame = [&reader](Frame* frame) {
+                return (reader >> frame->functionIndex)
+                    && (reader >> frame->fileIndex)
+                    && (reader >> frame->line);
+            };
+            if (readFrame(&ip.frame)) {
+                Frame inlinedFrame;
+                while (readFrame(&inlinedFrame)) {
+                    ip.inlined.push_back(inlinedFrame);
+                }
+            }
+
             instructionPointers.push_back(ip);
-            if (find(opNewStrIndices.begin(), opNewStrIndices.end(), ip.functionIndex) != opNewStrIndices.end()) {
+            if (find(opNewStrIndices.begin(), opNewStrIndices.end(), ip.frame.functionIndex) != opNewStrIndices.end()) {
                 IpIndex index;
                 index.index = instructionPointers.size();
                 opNewIpIndices.push_back(index);
@@ -449,8 +458,12 @@ POTENTIALLY_UNUSED void printTrace(const AccumulatedTraceData& data, TraceIndex 
         const auto trace = data.findTrace(index);
         const auto& ip = data.findIp(trace.ipIndex);
         cerr << index << " (" << trace.ipIndex << ", " << trace.parentIndex << ")" << '\t'
-             << data.stringify(ip.functionIndex) << " in " << data.stringify(ip.moduleIndex) << " at "
-             << data.stringify(ip.fileIndex) << ':' << ip.line << '\n';
+             << data.stringify(ip.frame.functionIndex) << " in " << data.stringify(ip.moduleIndex) << " at "
+             << data.stringify(ip.frame.fileIndex) << ':' << ip.frame.line << '\n';
+        for (const auto& inlined : ip.inlined) {
+            cerr << '\t' << data.stringify(inlined.functionIndex) << " at "
+                 << data.stringify(inlined.fileIndex) << ':' << inlined.line << '\n';
+        }
         index = trace.parentIndex;
     } while (index);
     cerr << "---\n";
@@ -496,10 +509,17 @@ void AccumulatedTraceData::diff(const AccumulatedTraceData& base)
             index.index = stringMap[index.index].index;
         }
     };
-    auto remapIp = [&remapString](InstructionPointer ip) -> InstructionPointer {
+    auto remapFrame = [&remapString](Frame frame) -> Frame {
+        remapString(frame.functionIndex);
+        remapString(frame.fileIndex);
+        return frame;
+    };
+    auto remapIp = [&remapString, &remapFrame](InstructionPointer ip) -> InstructionPointer {
         remapString(ip.moduleIndex);
-        remapString(ip.functionIndex);
-        remapString(ip.fileIndex);
+        remapFrame(ip.frame);
+        for (auto& inlined : ip.inlined) {
+            inlined = remapFrame(inlined);
+        }
         return ip;
     };
 

src/analyze/accumulatedtracedata.h
@@ -33,24 +33,42 @@
 #include "allocationdata.h"
 #include "util/indices.h"
 
-struct InstructionPointer
+struct Frame
 {
-    uint64_t instructionPointer = 0;
-    ModuleIndex moduleIndex;
     FunctionIndex functionIndex;
     FileIndex fileIndex;
     int line = 0;
 
+    bool operator==(const Frame& rhs) const
+    {
+        return std::tie(functionIndex, fileIndex, line)
+            == std::tie(rhs.functionIndex, rhs.fileIndex, rhs.line);
+    }
+
+    bool operator<(const Frame& rhs) const
+    {
+        return std::tie(functionIndex, fileIndex, line)
+             < std::tie(rhs.functionIndex, rhs.fileIndex, rhs.line);
+    }
+};
+
+struct InstructionPointer
+{
+    uint64_t instructionPointer = 0;
+    ModuleIndex moduleIndex;
+    Frame frame;
+    std::vector<Frame> inlined;
+
     bool compareWithoutAddress(const InstructionPointer& other) const
     {
-        return std::tie(moduleIndex, functionIndex, fileIndex, line)
-            < std::tie(other.moduleIndex, other.functionIndex, other.fileIndex, other.line);
+        return std::tie(moduleIndex, frame)
+             < std::tie(other.moduleIndex, other.frame);
     }
 
     bool equalWithoutAddress(const InstructionPointer& other) const
     {
-        return std::tie(moduleIndex, functionIndex, fileIndex, line)
-            == std::tie(other.moduleIndex, other.functionIndex, other.fileIndex, other.line);
+        return std::tie(moduleIndex, frame)
+            == std::tie(other.moduleIndex, other.frame);
     }
 };
 

src/analyze/print/heaptrack_print.cpp
@@ -152,11 +152,19 @@ struct Printer final : public AccumulatedTraceData
                                         auto node = findTrace(allocation.traceIndex);
                                         while (node.ipIndex) {
                                             const auto& ip = findIp(node.ipIndex);
-                                            if (isStopIndex(ip.functionIndex)) {
+                                            if (isStopIndex(ip.frame.functionIndex)) {
                                                 break;
                                             }
-                                            if (stringify(ip.functionIndex).find(filterBtFunction) != string::npos) {
+                                            auto matchFunction = [this](const Frame& frame) {
+                                                return stringify(frame.functionIndex).find(filterBtFunction) != string::npos;
+                                            };
+                                            if (matchFunction(ip.frame)) {
                                                 return false;
+                                            }
+                                            for (const auto& inlined : ip.inlined) {
+                                                if (matchFunction(inlined)) {
+                                                    return false;
+                                                }
                                             }
                                             node = findTrace(node.parentIndex);
                                         };
@@ -181,28 +189,36 @@ struct Printer final : public AccumulatedTraceData
     {
         printIndent(out, indent);
 
-        if (ip.functionIndex) {
-            out << prettyFunction(stringify(ip.functionIndex));
+        if (ip.frame.functionIndex) {
+            out << prettyFunction(stringify(ip.frame.functionIndex));
         } else {
             out << "0x" << hex << ip.instructionPointer << dec;
         }
 
         if (flameGraph) {
             // only print the file name but nothing else
-            if (ip.fileIndex) {
-                const auto& file = stringify(ip.fileIndex);
+            auto printFile = [this, &out](FileIndex fileIndex) {
+                const auto& file = stringify(fileIndex);
                 auto idx = file.find_last_of('/') + 1;
                 out << " (" << file.substr(idx) << ")";
+            };
+            if (ip.frame.fileIndex) {
+                printFile(ip.frame.fileIndex);
             }
             out << ';';
+            for (const auto& inlined : ip.inlined) {
+                out << prettyFunction(stringify(inlined.functionIndex));
+                printFile(inlined.fileIndex);
+                out << ';';
+            }
             return;
         }
 
         out << '\n';
         printIndent(out, indent + 1);
 
-        if (ip.fileIndex) {
-            out << "at " << stringify(ip.fileIndex) << ':' << ip.line << '\n';
+        if (ip.frame.fileIndex) {
+            out << "at " << stringify(ip.frame.fileIndex) << ':' << ip.frame.line << '\n';
             printIndent(out, indent + 1);
         }
 
@@ -212,6 +228,13 @@ struct Printer final : public AccumulatedTraceData
             out << "in ??";
         }
         out << '\n';
+
+        for (const auto& inlined : ip.inlined) {
+            printIndent(out, indent);
+            out << prettyFunction(stringify(inlined.functionIndex)) << '\n';
+            printIndent(out, indent + 1);
+            out << "at " << stringify(inlined.fileIndex) << ':' << inlined.line << '\n';
+        }
     }
 
     void printBacktrace(const TraceIndex traceIndex, ostream& out, const size_t indent = 0,
@@ -233,7 +256,7 @@ struct Printer final : public AccumulatedTraceData
             }
             skipFirst = false;
 
-            if (isStopIndex(ip.functionIndex)) {
+            if (isStopIndex(ip.frame.functionIndex)) {
                 break;
             }
 
@@ -254,7 +277,7 @@ struct Printer final : public AccumulatedTraceData
 
         const auto& ip = findIp(node.ipIndex);
 
-        if (!isStopIndex(ip.functionIndex)) {
+        if (!isStopIndex(ip.frame.functionIndex)) {
             printFlamegraph(findTrace(node.parentIndex), out);
         }
         printIp(ip, out, 0, true);
@@ -370,7 +393,7 @@ struct Printer final : public AccumulatedTraceData
         const auto ip = findIp(location);
 
         // skip anything below main
-        const bool shouldStop = isStopIndex(ip.functionIndex);
+        const bool shouldStop = isStopIndex(ip.frame.functionIndex);
         if (!shouldStop) {
             for (auto& merged : mergedAllocations) {
                 if (merged.leaked < 0) {
@@ -394,6 +417,7 @@ struct Printer final : public AccumulatedTraceData
             }
         }
 
+        // TODO: write inlined frames out to massif files
         printIndent(massifOut, depth, " ");
         massifOut << 'n' << (numAllocs + (skipped ? 1 : 0)) << ": " << heapSize;
         if (!depth) {
@@ -401,15 +425,15 @@ struct Printer final : public AccumulatedTraceData
                          "--alloc-fns, etc.\n";
         } else {
             massifOut << " 0x" << hex << ip.instructionPointer << dec << ": ";
-            if (ip.functionIndex) {
-                massifOut << stringify(ip.functionIndex);
+            if (ip.frame.functionIndex) {
+                massifOut << stringify(ip.frame.functionIndex);
             } else {
                 massifOut << "???";
             }
 
             massifOut << " (";
-            if (ip.fileIndex) {
-                massifOut << stringify(ip.fileIndex) << ':' << ip.line;
+            if (ip.frame.fileIndex) {
+                massifOut << stringify(ip.frame.fileIndex) << ':' << ip.frame.line;
             } else if (ip.moduleIndex) {
                 massifOut << stringify(ip.moduleIndex);
             } else {

src/interpret/heaptrack_interpret.cpp
@@ -64,11 +64,47 @@ string demangle(const char* function)
     return ret;
 }
 
-struct AddressInformation
+struct Frame
 {
+    Frame(string function = {}, string file = {}, int line = 0)
+        : function(function)
+        , file(file)
+        , line(line)
+    {}
+
+    bool isValid() const
+    {
+        return !function.empty();
+    }
+
     string function;
     string file;
-    int line = 0;
+    int line;
+};
+
+struct AddressInformation
+{
+    Frame frame;
+    vector<Frame> inlined;
+};
+
+struct ResolvedFrame
+{
+    ResolvedFrame(size_t functionIndex = 0, size_t fileIndex = 0, int line = 0)
+        : functionIndex(functionIndex)
+        , fileIndex(fileIndex)
+        , line(line)
+    {}
+    size_t functionIndex;
+    size_t fileIndex;
+    int line;
+};
+
+struct ResolvedIP
+{
+    size_t moduleIndex = 0;
+    ResolvedFrame frame;
+    vector<ResolvedFrame> inlined;
 };
 
 struct Module
@@ -88,22 +124,27 @@ struct Module
             return info;
         }
 
+        // try to find frame information from debug information
         backtrace_pcinfo(backtraceState, address,
                          [](void* data, uintptr_t /*addr*/, const char* file, int line, const char* function) -> int {
+                             Frame frame(demangle(function), file ? file : "", line);
                              auto info = reinterpret_cast<AddressInformation*>(data);
-                             info->function = demangle(function);
-                             info->file = file ? file : "";
-                             info->line = line;
+                             if (!info->frame.isValid()) {
+                                info->frame = frame;
+                             } else {
+                                info->inlined.push_back(frame);
+                             }
                              return 0;
                          },
                          [](void* /*data*/, const char* /*msg*/, int /*errnum*/) {}, &info);
 
-        if (info.function.empty()) {
+        // no debug information available? try to fallback on the symbol table information
+        if (!info.frame.isValid()) {
             backtrace_syminfo(
                 backtraceState, address,
                 [](void* data, uintptr_t /*pc*/, const char* symname, uintptr_t /*symval*/, uintptr_t /*symsize*/) {
                     if (symname) {
-                        reinterpret_cast<AddressInformation*>(data)->function = demangle(symname);
+                        reinterpret_cast<AddressInformation*>(data)->frame.function = demangle(symname);
                     }
                 },
                 [](void* /*data*/, const char* msg, int errnum) {
@@ -131,14 +172,6 @@ struct Module
     uintptr_t addressEnd;
     size_t moduleIndex;
     backtrace_state* backtraceState;
-};
-
-struct ResolvedIP
-{
-    size_t moduleIndex = 0;
-    size_t fileIndex = 0;
-    size_t functionIndex = 0;
-    int line = 0;
 };
 
 struct AccumulatedTraceData
@@ -186,6 +219,11 @@ struct AccumulatedTraceData
             m_modulesDirty = false;
         }
 
+        auto resolveFrame = [this](const Frame& frame)
+        {
+            return ResolvedFrame{intern(frame.function), intern(frame.file), frame.line};
+        };
+
         ResolvedIP data;
         // find module for this instruction pointer
         auto module =
@@ -194,9 +232,9 @@ struct AccumulatedTraceData
         if (module != m_modules.end() && module->addressStart <= ip && module->addressEnd >= ip) {
             data.moduleIndex = module->moduleIndex;
             const auto info = module->resolveAddress(ip);
-            data.fileIndex = intern(info.file);
-            data.functionIndex = intern(info.function);
-            data.line = info.line;
+            data.frame = resolveFrame(info.frame);
+            std::transform(info.inlined.begin(), info.inlined.end(), std::back_inserter(data.inlined),
+                           resolveFrame);
         }
         return data;
     }
@@ -253,10 +291,13 @@ struct AccumulatedTraceData
 
         const auto ip = resolve(instructionPointer);
         fprintf(stdout, "i %zx %zx", instructionPointer, ip.moduleIndex);
-        if (ip.functionIndex || ip.fileIndex) {
-            fprintf(stdout, " %zx", ip.functionIndex);
-            if (ip.fileIndex) {
-                fprintf(stdout, " %zx %x", ip.fileIndex, ip.line);
+        if (ip.frame.functionIndex || ip.frame.fileIndex) {
+            fprintf(stdout, " %zx", ip.frame.functionIndex);
+            if (ip.frame.fileIndex) {
+                fprintf(stdout, " %zx %x", ip.frame.fileIndex, ip.frame.line);
+                for (const auto& inlined : ip.inlined) {
+                    fprintf(stdout, " %zx %zx %x", inlined.functionIndex, inlined.fileIndex, inlined.line);
+                }
             }
         }
         fputc('\n', stdout);

tests/manual/CMakeLists.txt
@@ -17,3 +17,7 @@ add_executable(signals signals.cpp)
 target_link_libraries(signals ${CMAKE_THREAD_LIBS_INIT})
 
 add_executable(libc_leaks libc_leaks.c)
+
+set(CMAKE_BUILD_TYPE RelWithDebInfo)
+
+add_executable(inlining inlining.cpp)

tests/manual/inlining.cpp
@@ -1,0 +1,19 @@
+inline void __attribute__((always_inline)) asdf()
+{
+    new char[1234];
+}
+
+inline void __attribute__((always_inline)) bar()
+{
+    asdf();
+}
+
+inline void __attribute__((always_inline)) foo()
+{
+    bar();
+}
+
+int main()
+{
+    foo();
+}

commit eb9c84cf7ad73bf3065ce140dcd80898cb0fcd96

Restore the caching functionality in StringCache::location
The patch 1b9a59dc broke this by refactoring the code. It forgot
to store the non-cached value back in the faster lookup table.
See the original patch c370b7be for the advantage this fast-path
can bring.

src/analyze/gui/parser.cpp
@@ -74,7 +74,7 @@ struct StringCache
         // first try a fast index-based lookup
         auto& location = m_locationsMap[index];
         if (!location) {
-            return frameLocation(ip.frame, ip.moduleIndex);
+            location = frameLocation(ip.frame, ip.moduleIndex);
         }
         return location;
     }