38f69da19998f456776c9ca8ed64c4f8a3d7e08b
Milian Wolff <mail@milianw.de>
2017-08-29 16:20:09 +0200

    Explicitly hide zero-cost rows in the top proxy
    When we diff two files and the result shows no difference for
    a given metric, it's max cost will be 0. In these cases, the
    check for `cost < m_costThreshold` would fail for the zero cost
    rows, even though we always want to hide them.

--- src/analyze/gui/topproxy.cpp
+++ src/analyze/gui/topproxy.cpp
@@ -60,7 +60,9 @@ bool TopProxy::filterAcceptsRow(int source_row, const QModelIndex& source_parent
     }
     const auto index = sourceModel()->index(source_row, toSource(m_type));
     const auto cost = index.data(TreeModel::SortRole).toLongLong();
-    if (cost < m_costThreshold) {
+    // note: explicitly exclude zero values, which could show up when we diff files
+    //       and no change was observed (overall) for a given metric
+    if (!cost || cost < m_costThreshold) {
         // don't show rows that didn't leak anything, or didn't trigger any
         // temporary allocations
         // in general, hide anything that's not really interesting
