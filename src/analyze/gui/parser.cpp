496047cdfd1c77d1cea7b923fc8beb22dd2daac0
Daniel Vrátil <dvratil@kde.org>
2019-10-09 10:07:12 +0200

    Fix build against Qt without std::hash<QString> specialization
    Summary:
    std::hash specializations for some Qt types were introduced in
    Qt 5.14, it appears. To build against older Qt we need to specialize
    std::hash ourselves.
    
    Test Plan: Master builds against Qt 5.12.5
    
    Reviewers: mwolff
    
    Reviewed By: mwolff
    
    Differential Revision: https://phabricator.kde.org/D24491

--- src/analyze/gui/parser.cpp
+++ src/analyze/gui/parser.cpp
@@ -32,6 +32,16 @@
 
 using namespace std;
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+template<>
+struct hash<QString> {
+    std::size_t operator()(const QString &v) const noexcept
+    {
+        return qHash(v);
+    }
+};
+#endif
+
 // Only use this hash when filling in the cache
 struct CacheSymbolHash
 {
