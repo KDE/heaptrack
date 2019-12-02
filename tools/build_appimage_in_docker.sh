9c90967be5081702336e97725b61c3ea28117fa7
Milian Wolff <milian.wolff@kdab.com>
2019-12-02 17:44:37 +0100

    Copy recent elf.h into docker to get libunwind with zlib support

--- tools/build_appimage_in_docker.sh
+++ tools/build_appimage_in_docker.sh
@@ -6,6 +6,7 @@ if [ ! -d /tmp/heaptrack-appimage-artifacts ]; then
     mkdir /tmp/heaptrack-appimage-artifacts
 fi
 
+cp /usr/include/elf.h tools/
 sudo docker build -t heaptrack_appimage -f tools/Dockerfile . || exit 1
 sudo docker run -v /tmp/heaptrack-appimage-artifacts:/artifacts -it heaptrack_appimage
 mv /tmp/heaptrack-appimage-artifacts/heaptrack-x86_64.AppImage heaptrack-$(git describe)-x86_64.AppImage
