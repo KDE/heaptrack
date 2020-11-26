bc0be0d1fc8d4fbc74bd88390bb2e78813a46957
Milian Wolff <mail@milianw.de>
2020-11-26 14:03:04 +0100

    Also benchmark std::list for tree construction
    As-is, this lies somewhere in-between std::vector and QVector:
    
    ```
     Performance counter stats for '/home/milian/projects/build/heaptrack/tests/benchmarks/bench_tree std::list':
    
              6.227,71 msec task-clock:u              #    1,000 CPUs utilized
                     0      context-switches:u        #    0,000 K/sec
                     0      cpu-migrations:u          #    0,000 K/sec
             1.068.071      page-faults:u             #    0,172 M/sec
        23.672.061.063      cycles:u                  #    3,801 GHz                      (83,33%)
           142.641.882      stalled-cycles-frontend:u #    0,60% frontend cycles idle     (83,33%)
        17.758.938.284      stalled-cycles-backend:u  #   75,02% backend cycles idle      (83,33%)
        22.212.698.253      instructions:u            #    0,94  insn per cycle
                                                      #    0,80  stalled cycles per insn  (83,33%)
         4.492.589.760      branches:u                #  721,387 M/sec                    (83,33%)
            21.193.689      branch-misses:u           #    0,47% of all branches          (83,33%)
    
           6,228371570 seconds time elapsed
    
           5,293329000 seconds user
           0,909763000 seconds sys
    ```
    
    Peak RSS:
    
    ```
            Maximum resident set size (kbytes): 4278300
    ```

--- tests/benchmarks/bench_tree.cpp
+++ tests/benchmarks/bench_tree.cpp
@@ -20,6 +20,7 @@
 #include <array>
 #include <cstdint>
 #include <iostream>
+#include <list>
 #include <random>
 #include <vector>
 
@@ -60,28 +61,28 @@ std::vector<Trace> generateTraces()
     return traces;
 }
 
-namespace Vector {
+namespace Tree {
 template <template <typename...> class Container>
-struct VectorTree
+struct Node
 {
     AllocationData cost;
     uint64_t ip = 0;
-    const VectorTree* parent = nullptr;
-    Container<VectorTree> children;
+    const Node* parent = nullptr;
+    Container<Node> children;
 };
 
 template <template <typename...> class Container>
-Container<VectorTree<Container>> buildTree(const std::vector<Trace>& traces)
+Container<Node<Container>> buildTree(const std::vector<Trace>& traces)
 {
-    auto findNode = [](Container<VectorTree<Container>>* nodes, uint64_t ip) {
-        auto it = std::find_if(nodes->begin(), nodes->end(),
-                               [ip](const VectorTree<Container>& node) { return node.ip == ip; });
+    auto findNode = [](Container<Node<Container>>* nodes, uint64_t ip) {
+        auto it =
+            std::find_if(nodes->begin(), nodes->end(), [ip](const Node<Container>& node) { return node.ip == ip; });
         if (it != nodes->end())
             return it;
-        return nodes->insert(it, VectorTree<Container> {{}, ip, nullptr, {}});
+        return nodes->insert(it, Node<Container> {{}, ip, nullptr, {}});
     };
 
-    Container<VectorTree<Container>> ret;
+    Container<Node<Container>> ret;
     for (const auto& trace : traces) {
         auto* nodes = &ret;
         for (const auto& ip : trace) {
@@ -94,10 +95,10 @@ Container<VectorTree<Container>> buildTree(const std::vector<Trace>& traces)
 }
 
 template <template <typename...> class Container>
-uint64_t numNodes(const VectorTree<Container>& node)
+uint64_t numNodes(const Node<Container>& node)
 {
     return std::accumulate(node.children.begin(), node.children.end(), uint64_t(1),
-                           [](uint64_t count, const VectorTree<Container>& node) { return count + numNodes(node); });
+                           [](uint64_t count, const Node<Container>& node) { return count + numNodes(node); });
 }
 
 template <template <typename...> class Container>
@@ -106,38 +107,27 @@ std::pair<uint64_t, uint64_t> run(const std::vector<Trace>& traces)
     const auto tree = buildTree<Container>(traces);
     const auto totalNodes =
         std::accumulate(tree.begin(), tree.end(), uint64_t(0),
-                        [](uint64_t count, const VectorTree<Container>& node) { return count + numNodes(node); });
+                        [](uint64_t count, const Node<Container>& node) { return count + numNodes(node); });
     return {tree.size(), totalNodes};
-}
-}
-
-namespace QtTree {
-std::pair<uint64_t, uint64_t> run(const std::vector<Trace>& traces)
-{
-    return Vector::run<QVector>(traces);
-}
-}
-
-namespace StdTree {
-std::pair<uint64_t, uint64_t> run(const std::vector<Trace>& traces)
-{
-    return Vector::run<std::vector>(traces);
 }
 }
 
 enum class Tag
 {
-    Qt,
-    Std
+    QVector,
+    StdVector,
+    StdList
 };
 
 std::pair<uint64_t, uint64_t> run(const std::vector<Trace>& traces, Tag tag)
 {
     switch (tag) {
-    case Tag::Qt:
-        return QtTree::run(traces);
-    case Tag::Std:
-        return StdTree::run(traces);
+    case Tag::QVector:
+        return Tree::run<QVector>(traces);
+    case Tag::StdVector:
+        return Tree::run<std::vector>(traces);
+    case Tag::StdList:
+        return Tree::run<std::list>(traces);
     }
     Q_UNREACHABLE();
 }
@@ -145,16 +135,18 @@ std::pair<uint64_t, uint64_t> run(const std::vector<Trace>& traces, Tag tag)
 int main(int argc, char** argv)
 {
     if (argc != 2) {
-        std::cerr << "usage: bench_tree [qt|std]\n";
+        std::cerr << "usage: bench_tree [QVector|std::vector|std::list]\n";
         return 1;
     }
 
     const auto tag = [&]() {
         auto t = std::string(argv[1]);
-        if (t == "qt")
-            return Tag::Qt;
-        if (t == "std")
-            return Tag::Std;
+        if (t == "QVector")
+            return Tag::QVector;
+        if (t == "std::vector")
+            return Tag::StdVector;
+        if (t == "std::list")
+            return Tag::StdList;
         std::cerr << "unhandled tag: " << t << "\n";
         exit(1);
     }();
