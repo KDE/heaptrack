af6826958d72c51f22b3589876191fd47b2cf8b0
Adriaan de Groot <groot@kde.org>
2021-02-18 22:31:45 +0100

    Fix 32-bit (i386) FreeBSD build

--- src/track/heaptrack_inject.cpp
+++ src/track/heaptrack_inject.cpp
@@ -66,8 +66,18 @@ using Dyn = ElfW(Dyn);
 using Rel = ElfW(Rel);
 using Rela = ElfW(Rela);
 using Sym = ElfW(Sym);
+#if __WORDSIZE == 64
 using Sxword = ElfW(Sxword);
 using Xword = ElfW(Xword);
+#else
+// FreeBSD elf32.h doesn't define Elf32_Sxword or _Xword. This is used in struct
+// elftable, where it's used as a tag value. Our Elf32_Dyn uses Elf32_Sword there,
+// as does the Linux definition (and the standard); the El64_Dyn uses Sxword.
+//
+// Linux elf.h defines Elf32_Sxword as a 64-bit quantity, so let's do that
+using Sxword = int64_t;
+using Xword = uint64_t;
+#endif
 }
 
 void overwrite_symbols() noexcept;
