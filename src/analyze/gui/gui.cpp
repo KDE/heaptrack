4ef51851b64a891aa8268532c9c13d9731608f83
Milian Wolff <mail@milianw.de>
2023-07-21 18:40:50 +0200

    Don't set deprecated/implicit AA_UseHighDpiPixmaps on Qt6 builds

--- src/analyze/gui/gui.cpp
+++ src/analyze/gui/gui.cpp
@@ -51,7 +51,9 @@ void Q_DECL_UNUSED initRCCIconTheme()
 
 int main(int argc, char** argv)
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
 
     QApplication app(argc, argv);
     app.setStyle(new ProxyStyle);
