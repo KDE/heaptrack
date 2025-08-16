0d234cc9e517fa94bddd519d50a0c063e44de639
Selim Agirman <recepselimagirman@gmail.com>
2025-08-16 16:56:40 +0000

    fix containerized appimage builds for non-root installations

--- tools/build_appimage_in_docker.sh
+++ tools/build_appimage_in_docker.sh
@@ -6,5 +6,6 @@ mkdir -p output
 
 docker run -it --rm \
     -v $PWD:/github/workspace/ \
+    --user $(id -u):$(id -g) \
     heaptrack-appimage \
     /github/workspace/tools/build_appimage.sh /github/workspace /github/workspace/output/build-appimage
